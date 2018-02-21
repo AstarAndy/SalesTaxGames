d8689f93ad8a58920a8b6ca731843a440dea6ed7
Andy Faust <andyfaust@Andys-MacBook-Pro.local>
2018-02-21 18:15:51 -0500

    Still adding tests

--- src/main/java/com/acf/taxgames/SalesTaxGames.java
+++ src/main/java/com/acf/taxgames/SalesTaxGames.java
@@ -1,6 +1,12 @@
 package com.acf.taxgames;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -10,27 +16,32 @@ public class SalesTaxGames {
     public static void main(String... args) {
         SalesTaxGames stg = new SalesTaxGames();
 
-        String csvFileToProcess = "ReceiptDetails.csv";
+        String csvFileToProcess = "xxxReceiptDetails.csv";
         if (args.length > 0) {
             csvFileToProcess = args[0];
         }
 
         try {
             stg.execute(csvFileToProcess);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
 
     }
 
-    public void execute(String fileNameToLoad) throws IOException {
-        ;
+    public void execute(String fileNameToLoad) throws Exception {
+
+
+//        System.out.println(relativePath.toString());
+
     }
 
-
-
-    public Stream<String> getCsvLinesAsStream(String fileNameToLoad) throws IOException {
+    public Stream<String> getReceiptDetailsAsStream(String fileNameToLoad) throws Exception {
+        // If the file name is bad, or doesn't exist; then we'll get an NPE and assume the file name is bad
+        Path pathToReceiptsFile = Paths.get(getClass().getClassLoader().getResource(fileNameToLoad).toURI());
+        Stream<String> detailLines = Files.lines(pathToReceiptsFile);
+        detailLines.forEach(System.out::println);
         return null;
     }
 
